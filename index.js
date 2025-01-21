4f42c4d99a1882c97b10c2e903c0e111fde64db6
sarath R <lovelylovez143@gmail.com>
2025-01-21 17:33:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const io = require('socket.io')(8800,{
+    cors : {
+        origin: '*'
+    }
+})
+const axios = require('axios')
+
+let activeUsers = []
+
+io.on('connection',(socket)=>{
+
+    //Add new USer
+    socket.on('add-new-user',(newUserId)=>{
+        if(newUserId != null){
+            if(!activeUsers.some((user)=>user.userId === newUserId)){
+                activeUsers.push({
+                    userId: newUserId,
+                    socketId : socket.id
+                })
+            }
+        }
+        io.emit('get-users', activeUsers)
+    })
+
+    //send message
+    socket.on('send-message',(data)=>{
+        const {recieverId} = data
+        const user = activeUsers.find((user)=> user.userId === recieverId )
+        console.log('sending socket to :',recieverId)
+        console.log('Data :',data);
+        if(user){
+            io.to(user.socketId).emit('recieve-message',data)
+        }
+    })
+
+    //Disconnect users
+    socket.on('disconnect',async()=>{
+        console.log(activeUsers)
+        activeUsers.map((user)=>{
+            let requestBody = {
+                id: user?.userId,
+                lastseen: {
+                    date: new Date().toLocaleDateString('en-GB',{day:'2-digit',month:'2-digit',year:'numeric'}),
+                    time: new Date().toLocaleTimeString('en-US',{timeZone:'Asia/Kolkata',hour:'2-digit',minute:'2-digit',hour12:true})
+                }
+            }
+            const result = axios.post('http://localhost:3000/api/lastseen',requestBody)
+        })
+        activeUsers = activeUsers.filter((user)=> user.socketId !== socket.id)
+        console.log('user Disconnected',activeUsers);
+        io.emit('get-users',activeUsers)
+        io.emit('lastseen-time',new Date().toLocaleString())
+    })
+
+})
